4c6f167a1aa6ba0733de58013398b95fe20e4cdd
Nirob Saiful <kaisarsaiful@gmail.com>
2021-12-19 00:32:56 +0600

    first commit

---
+++ autowhatblast.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time 
+
+while True:
+    time.sleep(0.5) #set duration 
+    pyautogui.typewrite('I have Russian Friend!!!') # Type the message you wanna sent
+    pyautogui.press('enter') 
